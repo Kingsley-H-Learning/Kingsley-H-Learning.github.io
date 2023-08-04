80916eb1553a60ecff6199e99d2b6383c40c99e7
Kingsley <kingsley.huang@mycare.org.nz>
2023-08-05 11:14:00 +1200

    email sending exerise

--- html/src/index.js
+++ html/src/index.js
@@ -1,3 +1,2 @@
-console.log('index');
 var test = 'test';
 let test2 = 'test2';
--- html/src/index1.js
+++ html/src/index1.js
@@ -1,2 +1,19 @@
-console.log('test:', test);
-console.log('test1:', test1);
+// console.log('test:', test);
+// console.log('test1:', test1);
+
+var pokemon = {
+  firstname: 'Pika',
+  lastname: 'Chu ',
+  getPokeName: function () {
+    var fullname = this.firstname + ' ' + this.lastname;
+    return fullname;
+  },
+};
+
+var pokemonName = function () {
+  console.log(this.getPokeName() + 'I choose you!');
+};
+
+var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+
+logPokemon(); // 'Pika Chu I choose you!'
